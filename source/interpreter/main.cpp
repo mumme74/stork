d42116e71346b8e456fb7ee3ce06bddec39d13d9
Fredrik Johansson <fredrik@mumme.se>
2023-09-25 22:27:57 +0200

    Separate code to library and interpreter

--- source/interpreter/main.cpp
+++ source/interpreter/main.cpp
@@ -4,23 +4,25 @@
 
 int main() {
 	std::string path = __FILE__;
-	path = path.substr(0, path.find_last_of("/\\") + 1) + "test.stk";
-	
+	for (int i = 0; i < 3; ++i)
+		path = path.substr(0, path.find_last_of("/\\"));
+	path += "/examples/test.stk";
+
 	using namespace stork;
-	
+
 	stork_module m;
-	
+
 	add_standard_functions(m);
-	
+
 	m.add_external_function("greater", std::function<number(number, number)>([](number x, number y){
 		return x > y;
 	}));
-	
+
 	auto s_main = m.create_public_function_caller<void>("main");
-	
+
 	if (m.try_load(path.c_str(), &std::cerr)) {
 		s_main();
 	}
-	
+
 	return 0;
 }
